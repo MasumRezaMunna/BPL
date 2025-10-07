776e3992e36d1fec70eedd0b7de8fb618810457e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-07 22:03:02 +0600

    state management & toggle functionality

--- Vite/select-your-team/src/App.jsx
+++ Vite/select-your-team/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
@@ -13,7 +13,7 @@ const fetchPlayers = async () =>{
 
 
 function App() {
-
+    const [toggle, setToggle] = useState(true)
     const playerPromise = fetchPlayers()
 
   return (
@@ -21,14 +21,25 @@ function App() {
       
       <Navbar></Navbar>
 
-      
+      <div className=' max-w-[1200px] mx-auto flex justify-between items-center'>
+          <h1 className='font-bold text-2xl'>Available Players</h1>
 
-      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>
+          <div className='font-bold'>
+            <button onClick={() => setToggle(true)} className={`py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle===true?"bg-[#E7FE29] && text-black":""}`}>Available</button>
+            <button onClick={() => setToggle(false)} className={`py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0 ${toggle===false?"bg-[#E7FE29] && text-black":""}`}>Selected <span>(0)</span></button>
+          </div>
+      </div>
+
+      {
+        toggle === true?<Suspense fallback={<span className="loading loading-spinner loading-xl"></span>
 }>
         <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
-      </Suspense>
+      </Suspense> : <SelectedPlayers></SelectedPlayers>
+      }
 
-      {/* <SelectedPlayers></SelectedPlayers> */}
+      
+
+      
 
     </>
   )
