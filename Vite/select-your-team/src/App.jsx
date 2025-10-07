b1c90ec57f465c21ca4b9a97a09ab733a38cf563
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-07 18:32:35 +0600

    create navbar and create fake data for the players and load data

--- Vite/select-your-team/src/App.jsx
+++ Vite/select-your-team/src/App.jsx
@@ -1,53 +1,33 @@
-import { useState } from 'react'
 
+import { Suspense } from 'react'
 import './App.css'
+
+import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
+import Navbar from './components/Navbar/Navbar'
+import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
+
+const fetchPlayers = async () =>{
+  const res = await fetch('/players.json')
+  return res.json()
+}
+
 
 function App() {
 
+    const playerPromise = fetchPlayers()
 
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-      </div>
+      
+      <Navbar></Navbar>
+
+      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>
+}>
+        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
+      </Suspense>
+
+      {/* <SelectedPlayers></SelectedPlayers> */}
+
     </>
   )
 }
---
+++ Vite/select-your-team/src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+
+const AvailablePlayers = ({playerPromise}) => {
+    const playerData = use(playerPromise)
+    console.log(playerData)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AvailablePlayers;
